e15fb6bd966fa7fb3d9198fed60b39e57d417650
gokulmaxi <gokulakams@gmail.com>
2023-05-24 12:59:03 +0530

    [UPDATE]courosel with page bg completed

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -6,28 +6,37 @@ class CouroselPage {
     this.pageBg = "#FFFFFF"
   }
 }
+const indexTag = document.getElementById("index");
+const colorPicker = document.getElementById("pageColor");
 var x = 0;
 const pages = [new CouroselPage(1),new CouroselPage(2),new CouroselPage(3)]
-const indexTag = document.getElementById("index");
 function shiftRight() {
   console.log("Shifting Right");
   if(x == pages.length - 1 ){
     x= 0;
-    indexTag.innerHTML= pages[x].pageIndex;
   }else{
     x++;
-    console.log(x);
-    indexTag.innerHTML= pages[x].pageIndex;
   }
+  console.log(x);
+  indexTag.innerHTML= pages[x].pageIndex;
+  colorPicker.value = pages[x].pageBg
+  document.body.style.backgroundColor = pages[x].pageBg;
 }
 function shiftLeft() {
   console.log("Shifting Left");
   if(x == 0){
     x= pages.length - 1;
-    indexTag.innerHTML= pages[x].pageIndex;
   }else{
     x--;
-    console.log(x);
-    indexTag.innerHTML= pages[x].pageIndex;
   }
+  console.log(x);
+  indexTag.innerHTML= pages[x].pageIndex;
+  colorPicker.value = pages[x].pageBg
+  document.body.style.backgroundColor = pages[x].pageBg;
 }
+
+colorPicker.addEventListener("input",function () {
+  console.log(this.value);
+  pages[x].pageBg= this.value;
+  document.body.style.backgroundColor = this.value;
+})
